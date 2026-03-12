cedd7054ae929ad1ee058986e30ab0ab8d86821e
ajay-linuxMint_laptop <ajaysharma1jp@gmail.com>
2026-03-12 11:33:18 +0530

    SecureAllocation -v1

---
+++ model/SecureAllocator.h
@@ -0,0 +1,16 @@
+#ifndef SECUREALLOCATOR_H
+#define SECUREALLOCATOR_H
+
+#include<sodium.h>
+#include<cstddef>
+#include<new>
+#include<vector>
+#include<string>
+#include<cstdint>
+
+template <typename T>
+struct SecureAllocator{
+
+}
+
+#endif // SECUREALLOCATOR_H
